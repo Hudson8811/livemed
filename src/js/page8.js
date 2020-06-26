d1bc6793bde3347730b4949e5b13e350e1180730
PipZip <zipperpipper@gmail.com>
2020-06-26 16:23:45 +0600

    js

--- src/js/page8.js
+++ src/js/page8.js
@@ -1,11 +1,11 @@
-if ($('.h-section-3__video').length > 0) {
+if ($('.as-2__video').length > 0) {
     var vids = $("video");
     $.each(vids, function () {
         this.controls = false;
     });
 
-    $(".h-section-3__video video").click(function () {
-        var video = $(".h-section-3__video video")[0];
+    $(".as-2__video video").click(function () {
+        var video = $(".as-2__video video")[0];
         if (video.paused && !video.controls){
             $(".h-section-3__video-btn").attr("style", "display:none");
             video.controls = true;
@@ -20,7 +20,7 @@ if ($('.h-section-3__video').length > 0) {
     });
 
     $(".h-section-3__video-btn").click(function () {
-        var video = $(".h-section-3__video video")[0];
+        var video = $(".as-2__video video")[0];
         $(".h-section-3__video-btn").attr("style", "display:none");
         video.controls = true;
         video.play();
